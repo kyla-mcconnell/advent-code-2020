309103e68c0be91de82fd65617db71fb283d488a
kyla-mcconnell <kylamcconnell@live.com>
2020-12-08 23:48:41 -0500

    Update day 8 part 2

--- day8.py
+++ day8.py
@@ -1,27 +1,64 @@
-file = "data/input8_TEST.txt"
+f = open("data/input8_TEST.txt", "r")
+instruction_input = f.read().split("\n")
 
-def command_follower(filename):
-    with open(file, "r") as f:
-        commands = f.read().split("\n")
-        current_value = 0
-        index = 0
-        commands_visited = set()
-        
-        while index not in commands_visited:
-            commands_visited.add(index)
-            command, number = commands[index].split(" ") #retrieve command and value separately
-            number = int(number)
-            if command == "acc":
-                current_value += number
-                index += 1
-            elif command == "jmp":
-                index += number
-            elif command == "nop":
-                index += 1
-            else:
-                print("ERROR")
+def command_follower(commands):
+    current_value = 0
+    index = 0
+    commands_visited = set()
+    
+    while index not in commands_visited:
+        commands_visited.add(index)
+        command, number = commands[index].split(" ") #retrieve command and value separately
+        number = int(number)
+        if command == "acc":
+            current_value += number
+            index += 1
+        elif command == "jmp":
+            index += number
+        elif command == "nop":
+            index += 1
 
-        return current_value
+    return current_value
 
 #Part One: Where acc is the only command that adds to the current value, nop jumps 1 and jmp jumps the given number, return the final value before the program enters an infinite loop.
-print("Part One: The final value before the program enters an infinite loop is: " + str(command_follower(file)))
+print("Part One: The final value before the program enters an infinite loop is: " + str(command_follower(instruction_input)))
+
+def index_finder(commands):
+    current_value = 0
+    index = 0
+    problematic_index = "None"
+    new_input = commands.copy()
+
+    while problematic_index == "None":
+        new_input = commands.copy()
+        command, number = commands[index].split(" ") #retrieve command and value separately
+        number = int(number)
+        if command == "acc":
+            current_value += number
+            index += 1
+        elif command == "jmp":
+            new_input[index] = "nop " + str(number)
+            try:
+                number = command_follower(new_input)
+                index += number
+            except IndexError:
+                problematic_index = index
+        elif command == "nop":
+            new_input[index] = "jmp " + str(number)
+            try:
+                number = command_follower(new_input)
+                index += 1
+            except IndexError:
+                problematic_index = index
+
+    return command, number, problematic_index, new_input
+
+def index_fixer(commands):
+    command, number, problematic_input, new_input = index_finder(instruction_input)
+
+    return new_input
+
+print(index_fixer(instruction_input))
+
+
+f.close()
