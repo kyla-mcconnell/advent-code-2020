57478ae1932428bcdf30e3b00d3d9b1cfd93fd93
kyla-mcconnell <kylamcconnell@live.com>
2020-12-09 00:26:30 -0500

    Update day 8 p2

--- day8.py
+++ day8.py
@@ -1,12 +1,12 @@
 f = open("data/input8_TEST.txt", "r")
-instruction_input = f.read().split("\n")
+instruction_input = [line for line in f.read().split("\n") if line != ""]
 
 def command_follower(commands):
     current_value = 0
     index = 0
     commands_visited = set()
     
-    while index not in commands_visited:
+    while index not in commands_visited and index < len(commands):
         commands_visited.add(index)
         command, number = commands[index].split(" ") #retrieve command and value separately
         number = int(number)
@@ -23,13 +23,13 @@ def command_follower(commands):
 #Part One: Where acc is the only command that adds to the current value, nop jumps 1 and jmp jumps the given number, return the final value before the program enters an infinite loop.
 print("Part One: The final value before the program enters an infinite loop is: " + str(command_follower(instruction_input)))
 
-def index_finder(commands):
+def input_fixer(commands):
     current_value = 0
     index = 0
     problematic_index = "None"
     new_input = commands.copy()
 
-    while problematic_index == "None":
+    while problematic_index == "None" and index < len(commands):
         new_input = commands.copy()
         command, number = commands[index].split(" ") #retrieve command and value separately
         number = int(number)
@@ -51,14 +51,30 @@ def index_finder(commands):
             except IndexError:
                 problematic_index = index
 
-    return command, number, problematic_index, new_input
-
-def index_fixer(commands):
-    command, number, problematic_input, new_input = index_finder(instruction_input)
-
     return new_input
 
-print(index_fixer(instruction_input))
+print(command_follower(input_fixer(instruction_input)))
+
+# def re_reader(commands):
+#     current_value = 0
+#     value = 0
+#     index = 0
+        
+#     while index < len(commands) - 1:
+#         command, number = commands[index].split(" ") #retrieve command and value separately
+#         number = int(number)
+#         value = current_value
+#         if command == "acc":
+#             current_value += number
+#             index += 1
+#         elif command == "jmp":
+#             index += number
+#         elif command == "nop":
+#             index += 1
+
+#     return value
+
+# print(re_reader(input_fixer(instruction_input)))
 
 
 f.close()
